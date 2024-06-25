7b05e1401dfd13c85ffdf2c5f775d0a5b91f55ad
t-tsukano <user.name>
2024-06-25 15:16:14 +0900

    DB接続完了

--- BookManagement/BookManagement/Models/Book.cs
+++ BookManagement/BookManagement/Models/Book.cs
@@ -2,5 +2,12 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool BorrowedStatus { get; set; } = false;
+        public DateTime BorrowedDate { get; set; } = DateTime.Now;
+        public DateTime ReturnDate { get; set; }
+        public int PublishedYear { get; set; }
+        public int UserId { get; set; }
     }
 }
---
+++ BookManagement/BookManagement/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookManagement.Models
+{
+    public class User : IdentityUser<int>
+    {
+        // IdentityUserがもってるので省略
+    }
+}

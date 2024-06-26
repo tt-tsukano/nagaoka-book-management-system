7cc8bf55b543db4ddc3e5f551684c73b64cbf653
t-tsukano <user.name>
2024-06-26 09:50:10 +0900

    Homeコントローラーの編集

--- BookManagement/BookManagement/Controllers/HomeController.cs
+++ BookManagement/BookManagement/Controllers/HomeController.cs
@@ -1,32 +1,70 @@
+using BookManagement.Data;
 using BookManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BookManagement.Controllers
+namespace BookManagement.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    // データベースとのやりとりを担当するDbContextクラスを用意
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+        // コンストラクタでDbContextクラスを受け取る
+        _context = context;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    // Confirmアクション GET時
+    // 引数として該当するBookのidを受け取る
+    public IActionResult Confirm(int? id)
+    {
+        // idがnullの場合はエラーを返す
+        if (id == null)
         {
-            _logger = logger;
+            return NotFound();
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // idに該当するBookを取得
+        var book = _context.Books.FirstOrDefault(x => x.Id == id);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        // ビューにBookを渡して表示
+        return View(book);
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    // Confirmアクション POST時
+    // 引数として該当するBookのidを受け取る
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Confirm(int id)
+    {
+        // idに該当するBookを取得
+        var book = _context.Books.FirstOrDefault(x => x.Id == id);
+
+        // BookのBorrowedStatusをtrueに変更
+        book.BorrowedStatus = true;
+
+        // Indexアクションにリダイレクト
+        return RedirectToAction("Index");
     }
 }

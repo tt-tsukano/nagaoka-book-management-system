52d7009c67449b0bb252d350ee3f4d0b37507b67
y-shiga <y-shiga@s-giken.co.jp>
2024-06-26 18:40:50 +0900

    書籍一覧表示画面仮完成

--- BookManagement/BookManagement/Models/Book.cs
+++ BookManagement/BookManagement/Models/Book.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public bool BorrowedStatus { get; set; } = false;
-        public DateTime BorrowedDate { get; set; } = DateTime.Now;
-        public DateTime ReturnDate { get; set; }
+        public DateTime? BorrowedDate { get; set; } = DateTime.Now;
+        public DateTime? ReturnDate { get; set; }
         public int PublishedYear { get; set; }
         public int UserId { get; set; }
     }

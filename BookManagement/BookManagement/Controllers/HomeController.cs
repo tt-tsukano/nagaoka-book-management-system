5440bb90f1eca95201df3f62cf3e4c607be3d801
y-shiga <y-shiga@s-giken.co.jp>
2024-06-27 09:25:00 +0900

    書籍検索機能を追加

--- BookManagement/BookManagement/Controllers/HomeController.cs
+++ BookManagement/BookManagement/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using BookManagement.Data;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using BookManagement.ViewModels;
 
 namespace BookManagement.Controllers
 {
@@ -20,7 +21,33 @@ namespace BookManagement.Controllers
         public async Task<IActionResult> Index()
         {
             var bookslist = await _context.Books.ToListAsync();
-            return View(bookslist);
+
+            //TitleSearchViewModelのResultsに記事一覧を格納
+            var viewModel = new TitleSearchViewModel
+            {
+                Results = bookslist
+            };
+            
+            //bookslistではなく、viewModelをviewに返す
+            return View(viewModel);
+        }
+
+
+        //検索ボタンを押した後の操作
+        [HttpPost]
+        public async Task<IActionResult> Index(TitleSearchViewModel titleSearchViewModel)
+        {
+            IQueryable<Book> books = _context.Books;
+
+            //タイトルの全文一致
+            if (!string.IsNullOrEmpty(titleSearchViewModel.Title))
+            {
+                books = books.Where(b => b.Title.Contains(titleSearchViewModel.Title));
+            }
+
+            titleSearchViewModel.Results = await books.ToListAsync();
+
+            return View(titleSearchViewModel);
         }
 
         public IActionResult Privacy()

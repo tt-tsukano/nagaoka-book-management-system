58ff8a575d7b40a5e01731b148596cebc5c45a55
y-shiga <y-shiga@s-giken.co.jp>
2024-06-28 14:52:34 +0900

    書籍登録画面を追加

--- BookManagement/BookManagement/Migrations/20240628052830_AuthorColumn.Designer.cs
+++ BookManagement/BookManagement/Migrations/20240628052830_AuthorColumn.Designer.cs
@@ -4,6 +4,7 @@ using BookManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookManagement.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240628052830_AuthorColumn")]
+    partial class AuthorColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BookManagement/BookManagement/Models/Book.cs
+++ BookManagement/BookManagement/Models/Book.cs
@@ -1,12 +1,19 @@
-﻿namespace BookManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="タイトルを入力してください。")]
         public string Title { get; set; }
+        //Authorを追加
+        [Required(ErrorMessage ="著者名を入力してください。複数いる場合は、1名で良いです。")]
+        public string Author { get; set; }
         public bool BorrowedStatus { get; set; } = false;
         public DateTime? BorrowedDate { get; set; } = DateTime.Now;
         public DateTime? ReturnDate { get; set; }
+        [Required(ErrorMessage ="出版年を記入してください。")]
         public int PublishedYear { get; set; }
         public int UserId { get; set; }
     }

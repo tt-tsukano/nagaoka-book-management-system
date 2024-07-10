924949a397ef5545973d5acd3b870f0447495664
t-tsukano <user.name>
2024-07-10 15:42:12 +0900

    初めてのコミット

--- BookManagement/BookManagement/Controllers/HomeController.cs
+++ BookManagement/BookManagement/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace BookManagement.Controllers;
 
-[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
